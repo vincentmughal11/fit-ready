1e8d31490839a955cd625f2cf24b7fa5d503d650
vincentmughal11 <vincent.mughal77@gmail.com>
2024-12-19 22:05:15 -0600

    working on MLP with xgboost

--- mlp.py
+++ mlp.py
@@ -1,90 +1,62 @@
-import numpy as np
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputRegressor, MultiOutputClassifier
+from sklearn.metrics import mean_squared_error, accuracy_score
 
-class Perceptron:
-    def __init__(self, inputs, bias=1.0):
-        self.weights = (np.random.rand(inputs + 1) * 2) - 1
-        self.bias = bias
+import pandas as pd
+
+#set the model up
+data = pd.read_csv("data.csv")
+
+X = data.drop(columns=["Age", "Gender", "Weight (kg)", "Height (m)", "Session_Duration (hours)", "Workout_Type"])
+y = data[["Max_BPM", "Avg_BPM", "Resting_BPM"]]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+from xgboost import XGBRegressor
+
+model = MultiOutputRegressor(XGBRegressor(objective="reg:squarederror", max_depth=6, learning_rate=0.1, n_estimators=100))
+
+#train
+model.fit(X_train, y_train)
+
+#test/predict
+y_pred = model.predict(X_test)
+print("Predictions for regression:", y_pred)
+
+y_pred = model.predict(X_test)
+print("Predictions for classification:", y_pred)
+
+mse = mean_squared_error(y_test, y_pred, multioutput="raw_values")
+print("Mean Squared Errors for each target:", mse)
+
+import matplotlib.pyplot as plt
+
+xgb.plot_importance(model.estimators_[0])
+plt.show()
+
+# to tune hyperparameters
+
+# from sklearn.model_selection import GridSearchCV
+
+# param_grid = {
+#     "estimator__max_depth": [3, 5, 7],
+#     "estimator__learning_rate": [0.01, 0.1, 0.2],
+#     "estimator__n_estimators": [50, 100, 150],
+# }
+
+# # Use GridSearchCV with MultiOutputRegressor
+# grid_search = GridSearchCV(
+#     estimator=MultiOutputRegressor(XGBRegressor(objective="reg:squarederror")),
+#     param_grid=param_grid,
+#     scoring="neg_mean_squared_error",
+#     cv=3,
+#     verbose=1
+# )
+
+# grid_search.fit(X_train, y_train)
+# print("Best Parameters:", grid_search.best_params_)
 
 
-    def run(self, x):
-        x_sum = np.dot(np.append(x, self.bias), self.weights)
-        return self.sigmoid(x_sum)
-    
-    def set_weights(self, w_init):
-        self.weights = np.array(w_init)
 
-    def sigmoid(self, x):
-        return 1/(1+np.exp(-x))
-    
 
-class MultiLayerPerceptron:
-    def __init__(self, layers, bias = 1.0, eta = 0.5):
-        self.layers = np.array(layers,dtype=object)
-        self.bias = bias
-        self.eta = eta
-        self.network = []
-        self.values = []
-        self.d = []
-
-        for i in range(len(self.layers)):
-            self.values.append([])
-            self.d.append([])
-            self.network.append([])
-        
-            self.values[i] = [0.0 for j in range(self.layers[i])]
-            self.d[i] = [0.0 for j in range(self.layers[i])]
-            if i > 0:
-                for j in range(self.layers[i]): 
-                    self.network[i].append(Perceptron(inputs = self.layers[i-1], bias = self.bias))
-        
-        self.network = np.array([np.array(x) for x in self.network],dtype=object)
-        self.values = np.array([np.array(x) for x in self.values],dtype=object)
-        self.d = np.array([np.array(x) for x in self.d],dtype=object)
-
-    def set_weights(self, w_init):
-        for i in range(len(w_init)):
-            for j in range(len(w_init[i])):
-                self.network[i+1][j].set_weights(w_init[i][j])
-
-    def print_weights(self):
-        print()
-        for i in range(1,len(self.network)):
-            for j in range(self.layers[i]):
-                print("Layer",i+1,"Neuron",j,self.network[i][j].weights)
-        print()
-
-    def run(self, x):
-        x = np.array(x,dtype=object)
-        self.values[0] = x
-        for i in range(1,len(self.network)):
-            for j in range(self.layers[i]):  
-                self.values[i][j] = self.network[i][j].run(self.values[i-1])
-        return self.values[-1]
-    
-    def bp(self, x, y):
-        x = np.array(x,dtype=object)
-        y = np.array(y,dtype=object)
-
-        outputs = self.run(x)
-        
-        error = (y - outputs)
-        MSE = sum( error ** 2) / self.layers[-1]
-
-        self.d[-1] = outputs * (1 - outputs) * (error)
-
-        for i in reversed(range(1,len(self.network)-1)):
-            for h in range(len(self.network[i])):
-                fwd_error = 0.0
-                for k in range(self.layers[i+1]): 
-                    fwd_error += self.network[i+1][k].weights[h] * self.d[i+1][k]               
-                self.d[i][h] = self.values[i][h] * (1-self.values[i][h]) * fwd_error
-
-        for i in range(1,len(self.network)):
-            for j in range(self.layers[i]):
-                for k in range(self.layers[i-1]+1):
-                    if k==self.layers[i-1]:
-                        delta = self.eta * self.d[i][j] * self.bias
-                    else:
-                        delta = self.eta * self.d[i][j] * self.values[i-1][k]
-                    self.network[i][j].weights[k] += delta
-        return MSE
