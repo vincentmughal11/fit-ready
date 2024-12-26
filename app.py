c89a0835036a5437b0c3ca9adf9cba9201e9358c
vincentmughal11 <vincent.mughal77@gmail.com>
2024-12-26 14:05:45 -0600

    Refactor app.py to improve code readability, enhance model loading functions, and update user input handling

--- app.py
+++ app.py
@@ -1,34 +1,74 @@
+"""
+This file dictates the layout of the streamlit app. 
+It loads the user workout model and exercise recommender model 
+and displays the recommendations based on the user input.
+"""
+
 import streamlit as st
 import numpy as np
-from mlp import UserWorkoutModel, ExerciseRecommender
 import pandas as pd
+from mlp import UserWorkoutModel, ExerciseRecommender
+
 
 @st.cache_resource
 def load_user_workout_model():
+    """
+    This function loads the user workout model, 
+    preprocess the data from data/user_workout.csv, 
+    and then trains the model and returns it.
+    """
     user_workout_data = pd.read_csv("data/user_workout.csv")
-    user_workout_data = pd.get_dummies(user_workout_data, columns=["Gender", "Workout_Type"])
-    x1 = user_workout_data[["Age", "Gender_Female", "Gender_Male", "Weight (kg)", "Height (m)", "Session_Duration (hours)", "Workout_Type_Yoga", "Workout_Type_HIIT", "Workout_Type_Cardio", "Workout_Type_Strength"]]
+    user_workout_data = pd.get_dummies(
+        user_workout_data, columns=["Gender", "Workout_Type"]
+    )
+    x1 = user_workout_data[
+        [
+            "Age",
+            "Gender_Female",
+            "Gender_Male",
+            "Weight (kg)",
+            "Height (m)",
+            "Session_Duration (hours)",
+            "Workout_Type_Yoga",
+            "Workout_Type_HIIT",
+            "Workout_Type_Cardio",
+            "Workout_Type_Strength",
+        ]
+    ]
     y1 = user_workout_data[["Max_BPM", "Avg_BPM", "Resting_BPM"]]
-    userWorkoutModelCreator = UserWorkoutModel(x1, y1)
-    return userWorkoutModelCreator.train()
+    user_workout_model_creator = UserWorkoutModel(x1, y1)
+    return user_workout_model_creator.train()
+
 
 userWorkoutModel, X_test, y_test = load_user_workout_model()
 
+
 @st.cache_resource
 def load_exercise_recommender():
-    exerciseRecommender = ExerciseRecommender("data/exercises.csv")
-    exerciseRecommender.preprocess_data()
-    exerciseRecommender.train_model()
-    return exerciseRecommender
+    """
+    This funciton loads the exercise recommender model 
+    and processes the data from data/exercises.csv, 
+    trains the model, and then returns it.
+    """
+    recommender = ExerciseRecommender("data/exercises.csv")
+    recommender.preprocess_data()
+    recommender.train_model()
+    return recommender
 
-exerciseRecommender = load_exercise_recommender()
 
+exercise_recommender = load_exercise_recommender()
 
 
 # App Title
 st.title("FitReady")
 
+
 class UserInfoWorkout:
+    """
+    This class stores the user information and workout information 
+    and encodes it to be used in the model.
+    """
+
     def __init__(self, age, gender, weight, height, duration, workout):
         self.age = age
         self.gender = gender
@@ -38,15 +78,30 @@ class UserInfoWorkout:
         self.workout = workout
 
     def encode(self):
+        """
+        This function encodes the user information
+         and workout information to be used in the model.
+         """
         if self.workout == "Cardio":
-            workoutArray = [False, False, True, False]
+            workout_encode = [False, False, True, False]
         elif self.workout == "Strength":
-            workoutArray= [False, False, False, True]
+            workout_encode = [False, False, False, True]
         elif self.workout == "HIIT":
-            workoutArray = [False, True, False, False]
+            workout_encode = [False, True, False, False]
         elif self.workout == "Yoga":
-            workoutArray = [True, False, False, False]
-        return np.array([self.age, 0 if self.gender == "Male" else 1, 1 if self.gender == "Male" else 0,  self.weight, self.height, self.duration, *workoutArray])
+            workout_encode = [True, False, False, False]
+        return np.array(
+            [
+                self.age,
+                0 if self.gender == "Male" else 1,
+                1 if self.gender == "Male" else 0,
+                self.weight,
+                self.height,
+                self.duration,
+                *workout_encode,
+            ]
+        )
+
 
 # User Input
 st.sidebar.write("## User/Workout information")
@@ -54,25 +109,62 @@ user_gender = st.sidebar.selectbox("Gender", ["Male", "Female"])
 user_age = st.sidebar.slider("Age", 18, 65)
 user_height = st.sidebar.slider("Height (m)", 1.5, 2.2, step=0.01)
 user_weight = st.sidebar.slider("Weight (kg)", 50, 150)
-duration = st.sidebar.slider("Duration (hours)", 1.00, 3.00, step=0.01)
-workout_type = st.sidebar.selectbox("Workout Type", ["Cardio", "Strength", "HIIT", "Yoga"])
+user_duration = st.sidebar.slider("Duration (hours)", 1.00, 3.00, step=0.01)
+workout_type = st.sidebar.selectbox(
+    "Workout Type", ["Cardio", "Strength", "HIIT", "Yoga"]
+)
 
 
-#Workout input
-st.sidebar.write('## Exercises')
-st.sidebar.write('only for Strength workouts')
-muscle_groups = st.sidebar.multiselect("Target Muscles", ['Abdominals', 'Adductors', 'Abductors', 'Biceps', 'Calves', 'Chest', 'Forearms',
- 'Glutes', 'Hamstrings', 'Lats', 'Lower Back', 'Middle Back', 'Traps', 'Neck',
- 'Quadriceps', 'Shoulders', 'Triceps'])
-equipment = st.sidebar.multiselect("Equipment", ['Bands', 'Barbell', 'Kettlebells', 'Dumbbell', 'Other', 'Cable', 'Machine',
- 'Body Only', 'Medicine Ball', 'Exercise Ball', 'Foam Roll',
- 'E-Z Curl Bar'])
+# Workout input
+st.sidebar.write("## Exercises")
+st.sidebar.write("only for Strength workouts")
+muscle_groups = st.sidebar.multiselect(
+    "Target Muscles",
+    [
+        "Abdominals",
+        "Adductors",
+        "Abductors",
+        "Biceps",
+        "Calves",
+        "Chest",
+        "Forearms",
+        "Glutes",
+        "Hamstrings",
+        "Lats",
+        "Lower Back",
+        "Middle Back",
+        "Traps",
+        "Neck",
+        "Quadriceps",
+        "Shoulders",
+        "Triceps",
+    ],
+)
+equipment = st.sidebar.multiselect(
+    "Equipment",
+    [
+        "Bands",
+        "Barbell",
+        "Kettlebells",
+        "Dumbbell",
+        "Other",
+        "Cable",
+        "Machine",
+        "Body Only",
+        "Medicine Ball",
+        "Exercise Ball",
+        "Foam Roll",
+        "E-Z Curl Bar",
+    ],
+)
 
 
 # Display Recommendations
 st.subheader("Recommendations", divider="grey")
 
-user_info_workout = UserInfoWorkout(user_age, user_gender, user_weight, user_height, duration, workout_type)
+user_info_workout = UserInfoWorkout(
+    user_age, user_gender, user_weight, user_height, user_duration, workout_type
+)
 user_input = user_info_workout.encode()
 max_bpm, avg_bpm, rest_bpm = userWorkoutModel.predict([user_input])[0]
 
@@ -81,7 +173,7 @@ st.metric(label="Avg BPM", value=round(avg_bpm))
 st.metric(label="Resting BPM", value=round(rest_bpm))
 
 if workout_type == "Strength":
-    exercises = exerciseRecommender.recommend_exercises(muscle_groups, equipment)
+    exercises = exercise_recommender.recommend_exercises(muscle_groups, equipment)
     if not exercises:
         st.write("No exercises found for the selected muscle groups and equipment.")
     else:
--- mlp.py
+++ mlp.py
@@ -1,13 +1,37 @@
-import xgboost as xgb
-from sklearn.model_selection import train_test_split
-from sklearn.multioutput import MultiOutputRegressor, MultiOutputClassifier
-from sklearn.metrics import mean_squared_error, accuracy_score
-from xgboost import XGBRegressor
+'''
+This file states the two classes UserWorkoutModel and ExerciseRecommender.
+These classes are the machine learning models powering the app.
+'''
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBRegressor
+
+class ModelNotTrainedException(Exception):
+    """Custom exception for untrained model errors."""
+    def __init__(self, message="Model not trained. Call train_model() first."):
+        self.message = message
+        super().__init__(self.message)
+
 
 class UserWorkoutModel:
-    def __init__(self, x, y, max_depth=5, learning_rate=0.1, n_estimators=150, subsample=1, colsample_bytree=1, reg_alpha=0):
+    '''
+    This class is used to train a model that predicts the user's average, maximum,
+    and resting heart rate based on their metrics.
+    '''
+    def __init__(
+        self,
+        x,
+        y,
+        max_depth=5,
+        learning_rate=0.1,
+        n_estimators=150,
+        subsample=1,
+        colsample_bytree=1,
+        reg_alpha=0,
+    ):
         self.x = x
         self.y = y
         self.max_depth = max_depth
@@ -16,21 +40,27 @@ class UserWorkoutModel:
         self.subsample = subsample
         self.colsample_bytree = colsample_bytree
         self.reg_alpha = reg_alpha
-        
 
     def train(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.x, self.y, test_size=0.2, random_state=42)
-        model = MultiOutputRegressor(XGBRegressor(objective="reg:squarederror", 
-                                                  max_depth=self.max_depth, 
-                                                  learning_rate=self.learning_rate, 
-                                                  n_estimators=self.n_estimators, 
-                                                  subsample=self.subsample, 
-                                                  colsample_bytree=self.colsample_bytree, 
-                                                  reg_alpha=self.reg_alpha))
-        model.fit(X_train, y_train)
-        return model, X_test, y_test
-    
-
+        '''
+        This functions trains the ML model and returns the model, X_test, and y_test.
+        '''
+        x_train, x_test, y_train, y_test = train_test_split(
+            self.x, self.y, test_size=0.2, random_state=42
+        )
+        model = MultiOutputRegressor(
+            XGBRegressor(
+                objective="reg:squarederror",
+                max_depth=self.max_depth,
+                learning_rate=self.learning_rate,
+                n_estimators=self.n_estimators,
+                subsample=self.subsample,
+                colsample_bytree=self.colsample_bytree,
+                reg_alpha=self.reg_alpha,
+            )
+        )
+        model.fit(x_train, y_train)
+        return model, x_test, y_test
 
 
 # #test/predict
@@ -67,52 +97,79 @@ class UserWorkoutModel:
 # grid_search.fit(X_train, y_train)
 # print("Best Parameters:", grid_search.best_params_)
 
-from sklearn.ensemble import RandomForestClassifier
-
 class ExerciseRecommender:
+    '''
+    This class is used to recommend exercises based on user input.
+    '''
     def __init__(self, csv_path):
         self.data = pd.read_csv(csv_path)
         self.model = None
 
     def preprocess_data(self):
+        '''
+        Preprocess data into usable format for the model.
+        Achieves this by converting categorical data to numerical.
+        '''
         # Convert categorical data to numerical
-        self.data['Type'] = self.data['Type'].astype('category').cat.codes
-        self.data['BodyPart'] = self.data['BodyPart'].astype('category')
-        self.data['BodyPart_codes'] = self.data['BodyPart'].cat.codes
-        self.data['Equipment'] = self.data['Equipment'].astype('category')
-        self.data['Equipment_codes'] = self.data['Equipment'].cat.codes
-        self.data['Level'] = self.data['Level'].astype('category').cat.codes
-        self.data['Rating'] = self.data['Rating'].fillna(0)  # Fill missing ratings with 0
+        self.data["Type"] = self.data["Type"].astype("category").cat.codes
+        self.data["BodyPart"] = self.data["BodyPart"].astype("category")
+        self.data["BodyPart_codes"] = self.data["BodyPart"].cat.codes
+        self.data["Equipment"] = self.data["Equipment"].astype("category")
+        self.data["Equipment_codes"] = self.data["Equipment"].cat.codes
+        self.data["Level"] = self.data["Level"].astype("category").cat.codes
+        self.data["Rating"] = self.data["Rating"].fillna(
+            0
+        )  # Fill missing ratings with 0
 
     def train_model(self):
+        '''
+        Trains the model using the preprocessed data.
+        '''
         self.preprocess_data()
-        X = self.data[['Type', 'BodyPart_codes', 'Equipment_codes', 'Level']]
-        y = self.data['Title']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        x = self.data[["Type", "BodyPart_codes", "Equipment_codes", "Level"]]
+        y = self.data["Title"]
+        X_train, x_test, y_train, y_test = train_test_split(
+            x, y, test_size=0.2, random_state=42
+        )
         self.model = RandomForestClassifier(n_estimators=100, random_state=42)
         self.model.fit(X_train, y_train)
 
     def recommend_exercises(self, target_muscles, target_equipment, num_exercises=5):
+        '''
+        Recommends exercises based on target muscles and equipment.
+        '''
         if self.model is None:
-            raise Exception("Model not trained. Call train_model() first.")
-        
-        target_muscles_codes = [self.data['BodyPart'].cat.categories.get_loc(muscle) for muscle in target_muscles]
-        target_equipment_codes = [self.data['Equipment'].cat.categories.get_loc(equipment) for equipment in target_equipment]
-           
+            raise ModelNotTrainedException()
+
+        target_muscles_codes = [
+            self.data["BodyPart"].cat.categories.get_loc(muscle)
+            for muscle in target_muscles
+        ]
+        target_equipment_codes = [
+            self.data["Equipment"].cat.categories.get_loc(equipment)
+            for equipment in target_equipment
+        ]
+
         # Filter exercises by target muscles and equipment
-        filtered_data = self.data[self.data['BodyPart_codes'].isin(target_muscles_codes) & self.data['Equipment_codes'].isin(target_equipment_codes)]
+        filtered_data = self.data[
+            self.data["BodyPart_codes"].isin(target_muscles_codes)
+            & self.data["Equipment_codes"].isin(target_equipment_codes)
+        ]
         if filtered_data.empty:
             return []
 
         # Predict and recommend exercises
-        X = filtered_data[['Type', 'BodyPart_codes', 'Equipment_codes', 'Level']]
-        predictions = self.model.predict(X)
-        recommended_exercises = filtered_data[filtered_data['Title'].isin(predictions)].head(num_exercises)
-        
-        return recommended_exercises[['Title', 'Desc', 'BodyPart', 'Equipment', 'Rating']]
-    
+        x = filtered_data[["Type", "BodyPart_codes", "Equipment_codes", "Level"]]
+        predictions = self.model.predict(x)
+        recommended_exercises = filtered_data[
+            filtered_data["Title"].isin(predictions)
+        ].head(num_exercises)
+
+        return recommended_exercises[
+            ["Title", "Desc", "BodyPart", "Equipment", "Rating"]
+        ]
+
 
 # # Test the ExerciseRecommender
 data = pd.read_csv("data/exercises.csv")
-print(data[data['BodyPart'].isin(['Abductors'])])
-
+print(data[data["BodyPart"].isin(["Abductors"])])
